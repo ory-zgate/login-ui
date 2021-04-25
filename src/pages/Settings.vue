32dfbf82e7b294e574b765920166a37fb4693dcb
robbietree8 <wangzg1988@gmail.com>
2021-04-25 11:02:12 +0800

    feat: update profile in settings

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -13,9 +13,9 @@
           Settings
         </h2>
       </div>
-      <form class="mt-8 space-y-6" :action="action" method="POST">
+      <form class="mt-8 space-y-6" :action="passwordConfig.action" method="POST">
         <div class="rounded-md shadow-sm -space-y-px">
-          <div v-for="field in fields" :key="field">
+          <div v-for="field in passwordConfig.fields" :key="field">
             <div>
               <label :for="field.name" class="sr-only">{{ field.name }}</label>
               <input
@@ -36,7 +36,7 @@
             </div>
           </div>
 
-          <div v-for="message in messages" :key="message" class="text-red-500">
+          <div v-for="message in passwordConfig.messages" :key="message" class="text-red-500">
             {{ message.text }}
           </div>
         </div>
@@ -62,7 +62,61 @@
                 />
               </svg>
             </span>
-            Update
+            Update Password
+          </button>
+        </div>
+      </form>
+
+      <form class="mt-8 space-y-6" :action="profileConfig.action" method="POST">
+        <div class="rounded-md shadow-sm -space-y-px">
+          <div v-for="field in profileConfig.fields" :key="field">
+            <div>
+              <label :for="field.name" class="sr-only">{{ field.name }}</label>
+              <input
+                :name="field.name"
+                :type="field.type"
+                :required="field.required"
+                :value="field.value"
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                :placeholder="field.name"
+              />
+            </div>
+            <div
+              v-for="message in field.messages"
+              :key="message"
+              class="text-red-500"
+            >
+              <span>{{ message.text }}</span>
+            </div>
+          </div>
+
+          <div v-for="message in profileConfig.messages" :key="message" class="text-red-500">
+            {{ message.text }}
+          </div>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+              <!-- Heroicon name: solid/lock-closed -->
+              <svg
+                class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </span>
+            Update Profile
           </button>
         </div>
       </form>
@@ -77,18 +131,15 @@ export default {
   setup() {},
   data() {
     return {
-      action: "",
-      fields: [],
-      messages: [],
+      passwordConfig: {},
+      profileConfig: {}
     };
   },
   created() {
     initializeSettingsFlow()
       .then((response) => {
-        let passwordConfig = response.methods.password.config;
-        this.action = passwordConfig.action;
-        this.fields = passwordConfig.fields;
-        this.messages = passwordConfig.messages;
+        this.passwordConfig = response.methods.password.config;
+        this.profileConfig = response.methods.profile.config;
       })
       .catch((error) => {
         console.log(error);
