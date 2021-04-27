166beed202b7179a95a83b21102cfb5b4ea9d642
robbietree8 <wangzg1988@gmail.com>
2021-04-27 17:03:31 +0800

    feat: remove useless admin url

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,11 +1,9 @@
 export const KRATOS_BROWSER_URL = window.KRATOS_BROWSER_URL || "http://127.0.0.1:4433";
 export const KRATOS_PUBLIC_URL = window.KRATOS_PUBLIC_URL || "http://127.0.0.1:4433";
-export const KRATOS_ADMIN_URL = window.KRATOS_ADMIN_URL || "http://127.0.0.1:4434";
 
 export default {
   kratos: {
     browser: KRATOS_BROWSER_URL,
-    admin: KRATOS_ADMIN_URL,
     public: KRATOS_PUBLIC_URL,
     initLoginPath: KRATOS_BROWSER_URL + '/self-service/login/browser',
     initRegistrationPath: KRATOS_BROWSER_URL + '/self-service/registration/browser',
